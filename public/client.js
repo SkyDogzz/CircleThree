82dcc56bedef56eda929fb497e6308dbb33109d1
skydogzz <thomas.stephan@live.fr>
2021-11-05 14:01:12 +0100

    Commit

--- public/client.js
+++ public/client.js
@@ -1,15 +1,16 @@
 import * as THREE from "three";
-import { TetrahedronGeometry } from "three";
+import { OrbitControls } from "/jsm/controls/OrbitControls.js";
 import Stats from "/jsm/libs/stats.module.js";
 
 let scene, camera, renderer;
 let stats;
+let controls;
 
 init();
 animate();
 
 function init() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer = new THREE.WebGLRenderer({ antialias: true, vsync: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -20,18 +21,25 @@ function init() {
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    2000
+    4000
   );
-  camera.position.x = 1000
+  camera.position.x = 1
+  camera.position.y = 0
   camera.lookAt(0, 0, 0);
-  
-  const cardGeometry = new THREE.BoxGeometry(200, 200, 100);
-  const cardMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  const cardGeometry = new THREE.BoxGeometry(200, 200, 200);
+  const cardMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+  });
   for (let i = 0; i < 20; i++) {
-    const card = new THREE.Mesh(cardGeometry, cardMaterial)
-    card.position.x = 1000 * Math.cos(THREE.MathUtils.degToRad(i*20))
-    card.position.z = 1000 * Math.sin(THREE.MathUtils.degToRad(i*20))
-    scene.add(card)
+    const card = new THREE.Mesh(cardGeometry, cardMaterial);
+    card.position.x = 1000 * Math.cos(THREE.MathUtils.degToRad((360 / 20) * i));
+    card.position.z = 1000 * Math.sin(THREE.MathUtils.degToRad((360 / 20) * i));
+    card.rotation.y = -THREE.MathUtils.degToRad((i * 360) / 20 + 90);
+    card.material.wireframe;
+    scene.add(card);
   }
 
   stats = Stats();
@@ -50,8 +58,7 @@ function animate() {
   requestAnimationFrame(animate);
 
   stats.update();
-
-  scene.rotation.y += 0.02;
+  controls.update();
 
   renderer.render(scene, camera);
 }
